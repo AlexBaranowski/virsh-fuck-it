5da80d8a997092c4985e4522b82cf39353ec0f21
Alex Baranowski <alex@euro-linux.com>
2022-08-05 02:40:18 +0200

    second and final commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # virsh-fuck-it
+
+This script removes all domains and volumes with virsh. Use it when there is
+something broken beyond repair.
---
+++ virsh-fuck-it.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# author: Alex Baranowski
+# License: MIT
+
+
+set -euo pipefail
+
+
+purge_virsh_doms(){
+    for domain_name in $(virsh list --all --name ); do
+        virsh destroy --domain "$domain_name" || echo "domain already destroyed"
+        virsh undefine --domain "$domain_name" || echo "domain already undefined"
+    done
+}
+
+purge_virsh_vols(){
+    # virsh vol-list does not have option like --id or something like it xDDD <lol>
+    # tail -n +3 skip first two lines
+    for i in $(virsh vol-list --pool default | tail -n +3 | awk '{print $1}'); do 
+        echo "Removing $i libvirt volume"
+        virsh vol-delete --pool default "$i"
+    done
+}
+
+
+main(){
+    purge_virsh_doms
+    purge_virsh_vols
+}
+
+main
